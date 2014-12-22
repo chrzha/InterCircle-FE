7c6781490f1716aa6e5978fe08d9f0e0e57391d7
Chris Zhang - chrzha <chrzha@acxiom.com>
2014-12-22 17:07:44 +0800

    [Add feature] Add network check

--- app/src/main/java/com/acxiom/mobile/intercircle_fe/LoginActivity.java
+++ app/src/main/java/com/acxiom/mobile/intercircle_fe/LoginActivity.java
@@ -1,26 +1,43 @@
 package com.acxiom.mobile.intercircle_fe;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
 
-public class LoginActivity extends ActionBarActivity {
+public class LoginActivity extends Activity {
     private Button btnLogin;
     private TextView btnReg, btnForget;
     private Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//remove title
         setContentView(R.layout.activity_login);
+
+        ConnectivityManager con = (ConnectivityManager) getSystemService(Activity.CONNECTIVITY_SERVICE);
+        boolean wifi = con.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+                .isConnectedOrConnecting();
+        boolean internet = con.getNetworkInfo(ConnectivityManager.TYPE_MOBILE)
+                .isConnectedOrConnecting();
+
+        if (!internet&&!wifi) {
+            Toast.makeText(getApplicationContext(), "Please check your network!",
+                    Toast.LENGTH_LONG).show();
+        }
+
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnReg = (TextView)findViewById(R.id.btnReg);
         btnForget = (TextView)findViewById(R.id.btnForget);
